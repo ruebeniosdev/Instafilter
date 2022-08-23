e47763e83e8c4b99a5c923425fa8510e45fa6a68
Akankobateng Rueben <akankobatengr@gmail.com>
2022-08-23 02:23:21 +0000

    Initial Commit

---
+++ FillUp/FillUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  FillUpApp.swift
+//  FillUp
+//
+//  Created by RUEBEN on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct FillUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
